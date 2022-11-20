a3e5cfd3c59266e6ebe9bf175d4c2ea0e5a95908
1337h4x0r <1337h4x0r@gmx.de>
2022-11-20 11:39:32 +0100

    fix script dir lookup

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 DIR="$(dirname "$(realpath "$0")")"
+while [ -L "$DIR" ]; do
+	DIR="$(readlink $DIR)"
+done
 
 # copy files into the appropriate directories
 cp "$DIR/chkcryptoboot.conf" /etc/default/
--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 DIR="$(dirname "$(realpath "$0")")"
+while [ -L "$DIR" ]; do
+	DIR="$(readlink $DIR)"
+done
 
 # prepare file permissions
 find $DIR -type f -exec chmod go-rwx {} \;
